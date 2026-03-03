08da0f7cc0a6673bb1c24aaf79c4be543ba4abf7
andronk-lab <andronk@sunypoly.edu>
2026-03-03 12:35:57 -0500

    Update oly.py

--- oly.py
+++ oly.py
@@ -6,6 +6,13 @@ df = pd.read_csv("olympics_athletes_dataset.csv")
 
 st.title("Olympics Analytics: Age & Achievement")
 
+st.markdown("""
+**The Bettor's Edge:** This tool analyzes historical Olympic data from 1896 to 2024 to identify 
+winning athlete profiles. By adjusting the filters, the user can isolate specific demographics to find 
+high probability patterns, such as "peak performance ages" or "ideal physical statures" that 
+traditional betting markets might overlook.
+""")
+
 st.sidebar.header("Filter Results")
 
 min_age = int(df["age"].min())
@@ -47,6 +54,15 @@ with col4:
 
 
 st.divider()
-st.markdown("### Data Documentation")
-st.write("**Source:** Olympics Athletes Dataset (1896-2024)")
-st.write(f"**Sustainability:** Filters for Medal and Age allow for deeper cohort analysis as new datasets are added.")
+st.markdown(f"""
+
+**Analytical Focus:** This project addresses core betting questions: Which genders and countries dominate specific age cohorts? Is there a perfect physical stature for medal winners? And what is the precise age range for peak performance? Basically, what information can help me place a winning bet whether it be on an underdog or first place winner?
+
+**Data Source:** This dashboard utilizes the *Olympics Athletes Dataset (1896-2024)*, sourced from Kaggle at this link, https://www.kaggle.com/datasets/ashyou09/olympics-athletes-dataset-18962024. 
+**Date Accessed:** 2/26/2026  
+
+**Sustainability:** To keep this dashboard current as new results are released:
+1. Download the updated dataset from Kaggle.
+2. Ensure the file is named `olympics_athletes_dataset.csv` and placed in the root directory.
+3. Upon committing the new file to GitHub, the Streamlit app will automatically rerun calculations, updating all charts and filters without requiring code changes.
+""")
